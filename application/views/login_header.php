f09df23e0255f304198fb3850b7f058c22b1aa73
Travis Rigg <rigg.travis@gmail.com>
2014-02-27 16:53:37 -0500

    Include the bootstrap css file in the login header
    I forgot to include the css file here here as well. That is something that I do a lot.

--- application/views/login_header.php
+++ application/views/login_header.php
@@ -216,7 +216,7 @@
     <link
     	rel="styleship"
     	type="text/css"
-    	href="<?php echo base_url() . 'assets/bootstrap-3.1.1-dist'; ?>"
+    	href="<?php echo base_url() . 'assets/bootstrap-3.1.1-dist/css/boostrap.css'; ?>"
     	type="text/css"
     />
   </head>
