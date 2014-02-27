754ce7653c05bcdec140e8db737f49a3aec0feea
Travis Rigg <rigg.travis@gmail.com>
2014-02-27 16:48:17 -0500

    Linked bootstrap in the general header file

--- application/views/header.php
+++ application/views/header.php
@@ -5,5 +5,11 @@
 		<title><?php echo $title; ?></title>
 		<!--Bootstrap requires that the encoding be UTF-8-->
 		<meta charset="UTF-8" />
+		<link
+		    rel="styleship"
+		    type="text/css"
+		    href="<?php echo base_url() . 'assets/bootstrap-3.1.1-dist'; ?>"
+		    type="text/css"
+		/>
 	</head>
 	<body>
