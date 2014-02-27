d6c68cd600aad9a6f38829c8010ce3583bfcf9b2
Travis Rigg <rigg.travis@gmail.com>
2014-02-27 18:37:35 -0500

    Put bootstrap in the footer
    That's where it belongs.

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,2 +1,7 @@
+<!--Bootstrap requires jQuery.-->
+<script src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
+
+<!--Link to the rest to the bootstrap.-->
+<script src="<?php echo base_url() . 'assets/bootstrap-3.1.1-dist/js/bootstrap.js'; ?>"></script>
 </body>
 </html>
