177d6d3034851483ee7eaeb46630c653f395ed6a
Travis Rigg <rigg.travis@gmail.com>
2014-02-27 12:32:42 -0500

    Added add a person link.
    This is not to say that I added the functionality. That will come in another commit.

--- application/views/professor_control_center.php
+++ application/views/professor_control_center.php
@@ -8,6 +8,8 @@
 	<?php echo anchor("professor/view_courses", "View Courses"); ?>
 	<!--view courses by term-->
 	<!--add person-->
+	<!--TODO: Add person is needed immediately for other functionality to work.
+	<?php echo anchor("professor/add_person", "Add a Person"); ?>
 	<!--search for person by lastname-->
 	<!--search group by partners-->
 </div>
