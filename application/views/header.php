c63619a83f2539afb886e6939eb193de4a62ce0d
Travis Rigg <rigg.travis@gmail.com>
2014-02-27 20:07:02 -0500

    Moved encoding meta tag to the top. Added viewport
    This was all done to ensure that things look better via bootstrap.

--- application/views/header.php
+++ application/views/header.php
@@ -2,9 +2,14 @@
 <!DOCTYPE html>
 <html lang="en">
 	<head>
-		<title><?php echo $title; ?></title>
 		<!--Bootstrap requires that the encoding be UTF-8-->
 		<meta charset="UTF-8" />
+		<meta
+			name="viewport"
+			content="width=device-width, initial-scale=1"
+		/>
+		
+		<title><?php echo $title; ?></title>
 		<link
 		    rel="styleship"
 		    type="text/css"
