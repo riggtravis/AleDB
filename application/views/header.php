7277a247e72e6dd8419828282f56f6a0505464bc
Travis Rigg <rigg.travis@gmail.com>
2014-02-27 20:56:18 -0500

    Removed a redundant text/css from header
    I don't think that's causing the error I'm having trouble with, but it is worth a shot.

--- application/views/header.php
+++ application/views/header.php
@@ -14,7 +14,6 @@
 		    rel="styleship"
 		    type="text/css"
 		    href="<?php echo base_url() . 'assets/bootstrap-3.1.1-dist/css/boostrap.css'; ?>"
-		    type="text/css"
 		/>
 	</head>
 	<body>
