1e0c460977233c79ed68709b1f05091c377620b2
Travis Rigg <rigg.travis@gmail.com>
2014-02-27 20:18:14 -0500

    Added container div to courses view
    This should make things look better using codeigniter.

--- application/views/courses.php
+++ application/views/courses.php
@@ -1,29 +1,31 @@
-<!--We need to create a table with two fields-->
-<table class="table">
-	<tr>
-		<th>Department</th>
-		<th>Course Number</th>
-	</tr>
-	<!--There will be multiple rows in this based on how many courses
-		there are.-->
-	<!--Linkify all of the course numbers in order to open the
-		group view-->
-	<?php $this->load->helper("url"); ?>
-	<?php foreach ($course as $c): ?>
+<div class="container">
+	<!--We need to create a table with two fields-->
+	<table class="table">
 		<tr>
-			<td>
-				<?php
-					$department = $c->Department;
-					echo $department;
-				?>
-			</td>
-			<td>
-				<?php 
-				$coursenum = $c->CourseNum;
-				echo anchor("professor/view_course/$department/$coursenum", 
-					$coursenum)
-				?>
-			</td>
+			<th>Department</th>
+			<th>Course Number</th>
 		</tr>
-	<?php endforeach; ?>
-</table>
+		<!--There will be multiple rows in this based on how many courses
+			there are.-->
+		<!--Linkify all of the course numbers in order to open the
+			group view-->
+		<?php $this->load->helper("url"); ?>
+		<?php foreach ($course as $c): ?>
+			<tr>
+				<td>
+					<?php
+						$department = $c->Department;
+						echo $department;
+					?>
+				</td>
+				<td>
+					<?php 
+					$coursenum = $c->CourseNum;
+					echo anchor("professor/view_course/$department/$coursenum", 
+						$coursenum)
+					?>
+				</td>
+			</tr>
+		<?php endforeach; ?>
+	</table>
+</div>
