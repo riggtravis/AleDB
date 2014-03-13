25085ff12c7a6484228e5a93eb57f3637ea806ca
Travis Rigg <rigg.travis@gmail.com>
2014-03-13 16:45:03 -0400

    Made things more bootstrappy

--- application/views/create_course.php
+++ application/views/create_course.php
@@ -10,26 +10,63 @@
 		//	requirements the data must meet.
 		// Add the bootstrap information
 		echo form_open("professor/course_validate", array('class' => 'form-horizontal', 'role' => 'form'));
-		
-		echo form_label('Department', 'department');
-		$department = array(
-			'department'	=> 'department',
-			'id'			=> 'department',
-			'value'			=> set_value('department'),
-			'name'			=> 'department',
-			'maxlength'		=> '5'
-		);
-		echo form_input($department);
-		
-		echo form_label('Course Number', 'coursenum');
-		$coursenum = array(
-			'coursenum'		=> 'coursenum',
-			'id'			=> 'coursenum',
-			'value'			=> set_value('000'),
-			'name'			=> 'coursenum',
-		);
-		echo form_input($coursenum);
-		echo form_submit('submit', 'Go');
+	?>
+	<div class="form-group">
+		<?php
+			echo form_label(
+				'Department', 
+				'department',
+				array(
+					'class' => 'col-sm-1 col-md-1 col-lg-1',
+				)
+			);
+
+			$department = array(
+				'department'	=> 'department',
+				'id'			=> 'department',
+				'value'			=> set_value('department'),
+				'name'			=> 'department',
+				'maxlength'		=> '5',
+				'class' => 'col-sm-1 col-md-1 col-lg-1'
+			);
+			echo form_input($department);	
+		?>
+	</div>
+	<div class="form-group">
+		<?php
+			echo form_label(
+				'Course Number', 
+				'coursenum',
+				array('class' => 'col-sm-1 col-md-1 col-lg-1')
+			);
+			$coursenum = array(
+				'coursenum'		=> 'coursenum',
+				'id'			=> 'coursenum',
+				'value'			=> set_value('000'),
+				'name'			=> 'coursenum',
+				'class' => 'col-sm-1 col-md-1 col-lg-1'
+			);
+			echo form_input($coursenum);
+		?>
+	</div>
+	<div class="form-group">
+		<?php
+			echo form_label(
+				'Submit',
+				'submit',
+				array('class' => 'col-sm-1 col-md-1 col-lg-1')
+			);
+			$go = array(
+				'submit'	=> 'submit',
+				'id'		=> 'submit',
+				'name'		=> 'submit',
+				'value'		=> 'Go',
+				'class' => 'col-sm-1 col-md-1 col-lg-1'
+			);
+			echo form_submit($go);
+		?>
+	</div>
+	<?php
 		echo form_close();
 	?>
 </div>
