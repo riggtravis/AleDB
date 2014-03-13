ba3fc6242a439d6bd74f007f06e552c5f4b1cd4d
Travis Rigg <rigg.travis@gmail.com>
2014-03-13 16:01:37 -0400

    Links are now buttons
    that's what all the cool kids are doing these days.

--- application/views/professor_control_center.php
+++ application/views/professor_control_center.php
@@ -3,14 +3,33 @@
 		<?php $this->load->helper('url'); ?>
 		
 		<!--create course-->
-		<?php echo anchor("professor/create_course", "Create a Course"); ?>
+		<?php 
+			echo anchor(
+				"professor/create_course", 
+				"Create a Course",
+				array('class' => 'btn btn-default', 'role' => 'button')
+			); 
+		?>
 		
 		<!--view courses-->
-		<?php echo anchor("professor/view_courses", "View Courses"); ?>
+		<?php 
+			echo anchor(
+				"professor/view_courses", 
+				"View Courses",
+				array('class' => 'btn btn-default', 'role' => 'button')
+			); 
+		?>
+		
 		<!--view courses by term-->
 		<!--add person-->
 		<!--TODO: Add person is needed immediately for other functionality to work.-->
-		<?php echo anchor("professor/add_person", "Add a Person"); ?>
+		<?php 
+			echo anchor(
+				"professor/add_person", 
+				"Add a Person",
+				array('class' => 'btn btn-default', 'role' => 'button')
+			); 
+		?>
 		<!--search for person by lastname-->
 		<!--search group by partners-->
 	</div>
