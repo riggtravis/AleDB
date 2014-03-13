ffa36eeb3535e27853aaa18e9cce38a16b00eb0f
Travis Rigg <rigg.travis@gmail.com>
2014-03-13 10:48:09 -0400

    Turned Control center to a navbar
    This is what we wanted all along.

--- application/views/professor_control_center.php
+++ application/views/professor_control_center.php
@@ -1,15 +1,17 @@
-<div id="topbar">
-	<?php $this->load->helper('url'); ?>
-	
-	<!--create course-->
-	<?php echo anchor("professor/create_course", "Create a Course"); ?>
-	
-	<!--view courses-->
-	<?php echo anchor("professor/view_courses", "View Courses"); ?>
-	<!--view courses by term-->
-	<!--add person-->
-	<!--TODO: Add person is needed immediately for other functionality to work.-->
-	<?php echo anchor("professor/add_person", "Add a Person"); ?>
-	<!--search for person by lastname-->
-	<!--search group by partners-->
+<div class="navbar navbar-default navbar-fixed-top" >
+	<div class="container">
+		<?php $this->load->helper('url'); ?>
+		
+		<!--create course-->
+		<?php echo anchor("professor/create_course", "Create a Course"); ?>
+		
+		<!--view courses-->
+		<?php echo anchor("professor/view_courses", "View Courses"); ?>
+		<!--view courses by term-->
+		<!--add person-->
+		<!--TODO: Add person is needed immediately for other functionality to work.-->
+		<?php echo anchor("professor/add_person", "Add a Person"); ?>
+		<!--search for person by lastname-->
+		<!--search group by partners-->
+	</div>
 </div>
